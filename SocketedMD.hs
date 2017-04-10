e682af3ba9b7f05573ec81d502ec266faba0471f
Chenhan Shen <shen@Chenhans-MacBook-Pro.local>
2017-04-10 09:27:19 -0400

    m: socketed cabal

---
+++ SocketedMD.hs
@@ -0,0 +1,38 @@
+{-# LANGUAGE OverloadedStrings #-}
+
+module Main where
+
+import Conduit (
+      ConduitM, MonadIO,
+      (.|), awaitForever, mapC, mapM_C, runConduitRes, liftIO, mapMC,
+      sourceHandle, sourceFileBS, stdoutC, runResourceT
+   )
+import qualified Data.Conduit.Binary as CB
+
+import Data.ByteString (ByteString)
+import Data.ByteString.Char8 (append, unpack)
+-- import Data.Text.Lazy (pack, unpack)
+
+import Text.Blaze.Html.Renderer.Text (renderHtml)
+import Text.Markdown (def, markdown, msXssProtect)
+
+import System.IO (stdin, stdout)
+
+-- render str = renderHtml $ markdown def { msXssPrxotect = False } (
+--       pack $ str ++ "<script>console.log('wha wha??')</script>"
+--    )
+
+-- render = renderHtml $ markdown def { msXssPrxotect = False }
+
+main :: IO ()
+main = runConduitRes
+   $ sourceHandle stdin
+   .| CB.lines
+   .| mapC unpack
+   .| awaitForever sourceFileBS
+   .| CB.lines
+   .| mapC (flip append "\n")
+   .| mapM_C (liftIO . print)
+   -- .| mapM_C (\x -> (liftIO . print) x)
+   -- .| mapM_C (\x -> runResourceT $ (liftIO . print) x)
+   -- .| stdoutC
